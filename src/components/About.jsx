1301297b58ab1ccd50471a42c3df6f116eebe225
iskenderovmuslum@gmail.com <muslimiskandarov@Muslims-MacBook-Pro.local>
2023-12-09 15:08:13 +0400

    lorem ipsum added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque sunt
+        aliquid tenetur dolor sit fugiat consequatur, possimus cum cupiditate
+        alias neque expedita vel nemo quia, obcaecati amet magni tempore
+        voluptatum.
+      </p>
+    </div>
+  );
 };
 
 export default About;
